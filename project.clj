8f793e37cd83ac14cf6e41d46eadf816e23e3974
Benedek Fazekas <benedek.fazekas@gmail.com>
2013-09-22 17:47:07 +0100

    bumps up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded-uberrepl/lein-template "0.2.0"
+(defproject reloaded-uberrepl/lein-template "0.2.1-SNAPSHOT"
   :description "A Leiningen 2 template to generate project skeletons using
   tools.namespace and a :dev profile in an uberrepl compliant way. Based on Stuart
   Sierra's reloaded."
